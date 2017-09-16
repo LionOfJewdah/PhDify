a37f0090d64d4697e393399dba21ac0999b812e7
David Paul Silverstone <dsilvers@usc.edu>
2017-09-16 10:07:50 -0700

    Update!

--- append-title/append-title.js
+++ append-title/append-title.js
@@ -1,36 +1,36 @@
 function joinRegExpsWithDelim(delim = "|", ...rest) {
-    var components = [],
-        arg;
+	var components = [],
+		arg;
 
-    for (var i = 0; i < rest.length; i++) {
-        arg = rest[i];
-        if (arg instanceof RegExp) {
-            components = components.concat(arg._components || arg.source);
-        }
-    }
+	for (var i = 0; i < rest.length; i++) {
+		arg = rest[i];
+		if (arg instanceof RegExp) {
+			components = components.concat(arg._components || arg.source);
+		}
+	}
 
-    var combined = new RegExp("(?:" + components.join(")" + delim + "(?:") + ")");
-    combined._components = components; // For chained calls to "or" method
-    return combined;
+	var combined = new RegExp("(?:" + components.join(")" + delim + "(?:") + ")");
+	combined._components = components; // For chained calls to "or" method
+	return combined;
 };
 
 RegExp.any = joinRegExpsWithDelim.bind(null, "|");
 RegExp.sequence = joinRegExpsWithDelim.bind(null, "");
 
 RegExp.prototype.or = function() {
-    var args = Array.prototype.slice.call(arguments);
-    return RegExp.any.apply(null, [this].concat(args));
+	var args = Array.prototype.slice.call(arguments);
+	return RegExp.any.apply(null, [this].concat(args));
 };
 
 RegExp.prototype.chain = function () {
 	var args = Array.prototype.slice.call(arguments);
-    return RegExp.sequence.apply(null, [this].concat(args));
+	return RegExp.sequence.apply(null, [this].concat(args));
 };
 
 RegExp.prototype.followedBy = function (regex) {
 	// var sauce = regex;
 	// if (regex instanceof RegExp)
-	// 	sauce = regex.source;
+	//	 sauce = regex.source;
 	return this.chain(new RegExp("(?=" + regex.source + ")"));
 };
 
@@ -64,18 +64,18 @@ RegExp.prototype.makeCaseInsensitive = function () { return ignoreCase(this); };
 
 RegExp.prototype.makeGlobalAndCaseInsensitive = function() { return ignoreCaseGlobally(this); };
 
-var miller_regex = /(J\.?|Jeff|Jeffrey)\s+Miller/gi;
-var miller_regex_lower = /j\.?[_\s]*miller/gi;
-var miller_comma = /(Miller)(,\s*J(\.|eff(rey)?)?)/g;
+var miller_regex = /(J.?|Jeff|Jeffrey)\s+Miller/gi;
+var miller_regex_lower = /j.?[_\s]*miller/gi;
+var miller_comma = /(Miller)(,\s*J(.|eff(rey)?)?)/g;
 
-var PHD_MATCH = /(?:(_*|,\s*))?Ph\.?[^\S\n]*D/gi;
+var PHD_MATCH = /(?:(_*|,\s*))?Ph.?[^\Sn]*D/gi;
 var untitled_miller_regex = miller_regex.notFollowedBy(PHD_MATCH).makeGlobalAndCaseInsensitive();
-//console.log("untitled miller is", untitled_miller_regex);
+console.log("untitled miller is: " + untitled_miller_regex);
 var untitled_miller_lower = miller_regex_lower.notFollowedBy(PHD_MATCH).makeCaseInsensitive();
 
 
 function macroAppend(text, suffix) {
-	console.log("Appending", "\"" + suffix + "\"", "to", "\'" + text + "\'.");
+	//console.log("Appending \"" + suffix + '\" to \'' + text + "\'.");
 	return text + suffix;
 }
 
@@ -88,12 +88,12 @@ function append_phd(text) {
 }
 
 function insertTextAfterFirstMatch(text, infix, p1, p2) {
-	console.log("Inserting", "\"" + infix + "\"", "into", "\'" + text + "\'."); 
+	//console.log("Inserting", "\"" + infix + "\"", "into", "'" + text + "'."); 
 	// return text.replaceAll(regex, "$1 " + infix + "$2");
 	return p1 + ' ' + infix + p2;
 }
 
-// window.console.log("LEEE");
+window.console.log("LEEE");
 
 function elt(tagName, class_, text, attributes) {
 	var dom = document.createElement(tagName);
@@ -104,7 +104,8 @@ function elt(tagName, class_, text, attributes) {
 	for (var attr in attributes) {
 		if (attributes.hasOwnProperty(attr)) {
 			dom.setAttribute(attr, attributes[attr]);
-		} /*
+		} 
+		/*
 		else {
 			console.log(attr, attributes[attr]);
 		}
@@ -117,10 +118,10 @@ function findOrMakeElement(query, tagName, class_, text, attributes) {
 	return document.querySelector(query) || elt(tagName, class_, text, attributes);
 }
 
-window.document.body.appendChild(findOrMakeElement("#found", "h1", "big", "IT MATCHES THIS PAGE", {id: "found"}));
+// window.document.body.appendChild(findOrMakeElement("#found", "h1", "big", "IT MATCHES THIS PAGE", {id: "found"}));
 
 var currentNode,
-    ni = document.createNodeIterator(document.documentElement, NodeFilter.SHOW_ELEMENT);
+	ni = document.createNodeIterator(document.documentElement, NodeFilter.SHOW_ELEMENT);
 
 while (currentNode = ni.nextNode()) {
 	if (!(currentNode.children[0])) {
@@ -131,7 +132,8 @@ while (currentNode = ni.nextNode()) {
 
 function PhDify(str) {
 	return str.replace(untitled_miller_regex, appendPhDSimple)
-		.replace(untitled_miller_lower, append_phd);
-		.replace(insertTextAfterFirstMatch)
+		.replace(untitled_miller_lower, append_phd)
+		.replace(insertTextAfterFirstMatch);
 		// .replace(miller_comma, function(match, p1, p2) { return p1 + " Ph.D." + p2;})
 }
+
